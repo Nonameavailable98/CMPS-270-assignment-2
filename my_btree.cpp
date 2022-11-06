5e67c0cd33d30f8d42b8bceeb4249f2d94be0d64
Bilal Osman <bno00@mail.aub.edu>
2022-11-06 21:32:03 +0200

    first commit

---
+++ my_btree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#define SIZE 50
+
+template <class T>
+class BinaryTree
+{
+
+	struct treeNode{
+		T data;
+		struct treeNode* right = nullptr;
+		struct treeNode* left = nullptr;
+	};
+
+};
+int main(){
+    return 0;
+}

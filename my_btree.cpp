43e3cfee8e615804d703d208d49acea1dd8b3609
Nonameavailable98 <112321425+Nonameavailable98@users.noreply.github.com>
2022-11-06 21:45:36 +0200

    added contructor and destructor

--- my_btree.cpp
+++ my_btree.cpp
@@ -12,7 +12,38 @@ class BinaryTree
 		struct treeNode* left = nullptr;
 	};
 
+    public:
+	BinaryTree();
+	~BinaryTree();
+
+    private:
+	struct treeNode* root;
+	int treeSize;
+
 };
+
+// requires:
+// nothing
+// effect:
+// contruct a new BinaryTree Object
+
+
+
+template <class T>
+BinaryTree<T>::BinaryTree(){
+	this->root = NULL;
+	this->treeSize = 0;
+}
+
+// requires:
+// existence of BinaryTree object
+// effect:
+// destructs calling object
+template <class T>
+BinaryTree<T>::~BinaryTree(){
+	deleteTree(this->root);
+}
+
 int main(){
     return 0;
 }
